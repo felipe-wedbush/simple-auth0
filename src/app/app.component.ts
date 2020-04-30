90fa3f359af086fbdb6ecb4e03978b63045635ac
Felipe Lee <LeeF@wedbush.com>
2020-04-30 12:20:04 -0700

    getting env files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Environment } from '../environments/environment-variables';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-auth0';
+  clientId = Environment.clientId;
+  auth0Domain = Environment.auth0Domain;
+  DONT_EXIST = Environment.DONT_EXIST;
+  MYAPP_DEFAULT_SETTING = Environment.MYAPP_DEFAULT_SETTING;
 }
