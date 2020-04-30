fb14d01f6ab9cc9e5c7b3ebc025734887a4625f4
Felipe Lee <LeeF@wedbush.com>
2020-04-30 13:27:23 -0700

    trying to see env files

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,7 +3,7 @@ const keyPrefix = 'MYAPP_';
 const keys = Object.keys(process.env).filter((key) => key.startsWith(keyPrefix));
 let env = {};
 keys.forEach(key => env[key] = JSON.stringify(process.env[key]));
-// console.log('env=',env);
+console.log('env=',env);
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
